1a915caafe30484fb9ed0be7040feac29c5e7544
Rolando Mamani <mamanirolando2609@gmail.com>
2024-10-07 00:21:57 -0300

    modularizando?

--- js/js.js
+++ js/js.js
@@ -7,6 +7,7 @@ let tienda = [productos.producto1,productos.producto2,productos.producto3,produc
 
 let valorTotal = 0
 
+let descuentoUsado = false
 const codigoDescuento = "CoderHouse"
 const descuento = 0.5
 
@@ -16,22 +17,26 @@ function comprar(){
 
     let valor = 0
     let condicion = true
+    let multiplicar = 1 
+    let compra = 0
 
     do{
+        multiplicar = 1
+        
         alert("Productos \n1. producto1 : 10$ \n2. Producto2 : 20$ \n3. Producto3 : 30$ \n4. Producto4 : 40$\n5. Producto5 : 50$")
     
         const productoSelec = parseInt(prompt("Digite el numero del producto que quiere comprar: "))
-    
-        valor += tienda[productoSelec - 1 ]
-    
-        alert("usted eligio el producto de valor: " + tienda[productoSelec - 1 ])
-    
-        let seguir = prompt("Quiere seguir comprando? si/no:")
-        
-        if(seguir == "no"){
-            condicion = false
-        }
 
+        multiplicar = parseInt(prompt("Cuantas unidades quieres del producto: "))
+
+        compra = tienda[productoSelec - 1 ] * multiplicar
+        valor += compra
+
+        alert("Su compra hecha es de: " + compra)
+        
+
+        condicion = funcionSino()
+  
         alert(compraInfo + valor)
 
     } while(condicion)
@@ -39,7 +44,23 @@ function comprar(){
     return valor
 }
 
-function funcionDescuento( valor ){
+function funcionSino() {
+    let seguir;
+    
+    while (true) {
+        seguir = prompt("¿Quiere seguir comprando? si/no:");
+
+        if (seguir == "no") {
+            return false;
+        } else if (seguir == "si") {
+            return true;
+        } else {
+            alert("Responda correctamente...");
+        }
+    }
+}
+
+function funcionDescuento( valor, descuentoUsado ){
 
     const codigo = prompt("ingrese un codigo de descuento: ")
 
@@ -51,6 +72,7 @@ function funcionDescuento( valor ){
     } else{
         alert("el codigo ingresado es incorrecto")
     }
+    
 
     return valor
 }
@@ -67,6 +89,7 @@ while(condicion){
             break;
 
         case 2: valorTotal = funcionDescuento(valorTotal)
+    
             break;
         
         case 3: alert(compraInfo + valorTotal)
